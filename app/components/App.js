a26ecfdda88f3a788953b6223511b3ab41abef73
zhangweidong <zwd89757@163.com>
2016-07-08 18:09:05 +0800

    添加组件

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { RouteHandler } from 'react-router';
+import Footer from './Footer';
 class App extends React.Component{
 	render (){
 		return(
 			<div>
 				<RouteHandler />
+				<Footer />
 			</div>
 		);
 	}
